45f039133fe03b5975445882e30c6b2144b92f91
kiranfatima14 <102468461+kiranfatima14@users.noreply.github.com>
2022-05-13 13:06:21 +0400

    Update Coustomers.java

--- Coustomers.java
+++ Coustomers.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Coustomers {
+public class Customers {
 	int custid;
 	String custname;
 	String coursecust;
